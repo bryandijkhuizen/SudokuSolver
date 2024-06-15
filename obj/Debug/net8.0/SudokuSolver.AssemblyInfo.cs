bfcf1dc9332beba1382ff094c1243ca7a1e171cd
Bryan Dijkhuizen <bryandijkh@icloud.com>
2024-06-15 19:27:27 +0200

    More files

--- obj/Debug/net8.0/SudokuSolver.AssemblyInfo.cs
+++ obj/Debug/net8.0/SudokuSolver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5803aa010eeb21742bd7a389e18c920cead4f258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe30da7baaffffb07a7422e0f9acfd6738e52886")]
 [assembly: System.Reflection.AssemblyProductAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SudokuSolver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
